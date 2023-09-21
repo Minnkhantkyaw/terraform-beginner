678ac448906495ea07432cf339ebf762d003e2c6
Shandra Morton <shanmortonbootcamp@gmail.com>
2023-09-21 22:34:00 +0000

    #10 Working inside TF create random name, use init, fmt, plan, apply.

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
-
 #First Change
 #Second Change
+
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "s3_bucket_name" {
+  length  = 16
+  special = false
+  #override_special = "/@£$" 
+}
+
+output "s3_bucket_name_id" {
+  description = "random S3 Bucket name"
+  value       = random_string.s3_bucket_name.id
+}
