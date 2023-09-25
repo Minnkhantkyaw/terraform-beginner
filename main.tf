73e8789dce3d99987ee71346c579e11ab52578fb
Shandra Morton <125090413+shanmorton-bootcamp@users.noreply.github.com>
2023-09-24 23:50:51 -0700

    #12 Install AWS provider, make and destroy bucket with random name (#13)

--- main.tf
+++ main.tf
@@ -7,7 +7,15 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -17,10 +25,23 @@ provider "random" {
 resource "random_string" "s3_bucket_name" {
   length  = 16
   special = false
+  upper   = false
+  lower   = true
+  numeric = true
   #override_special = "/@£$" 
 }
 
 output "s3_bucket_name_id" {
   description = "random S3 Bucket name"
   value       = random_string.s3_bucket_name.id
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.s3_bucket_name.id
+
+  # tags = {
+  #   Name        = "My bucket"
+  #   Environment = "Dev"
+  # }
 }
