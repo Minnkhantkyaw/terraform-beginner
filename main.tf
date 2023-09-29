5c85e3090d8c488fcbc10fb1de7785aa9965a0a1
Shandra Morton <125090413+shanmorton-bootcamp@users.noreply.github.com>
2023-09-29 13:59:40 -0700

    28 tf import and config drift (#29)
    * Tf Import and Config Drift WIP no.1
    
    * Finished Tf Import and Config Drift video.

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-resource "random_string" "s3_bucket_name" {
-  length  = 16
-  special = false
-  upper   = false
-  lower   = true
-  numeric = true
-  #override_special = "/@£$" 
-}
 resource "aws_s3_bucket" "example" {
-  bucket = random_string.s3_bucket_name.id
+  bucket = var.s3_bucket_name
 
   tags = {
     UUID = var.UUID
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "s3_bucket_name" {
+  description = "Value that is unique."
+  value       = var.s3_bucket_name
+}
+
 output "UUID" {
   description = "Value that is unique."
   value       = var.UUID
