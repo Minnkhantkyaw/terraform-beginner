a927896016ff2b0f3c28c3fb366d8d374fa2336c
Shandra Morton <shanmortonbootcamp@gmail.com>
2023-10-14 01:09:49 +0000

    Implementing CRUD @ 1:09:29 in video, working no errors to this point.

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,7 +5,7 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:BoUzrbJKCJbjH+uawomos2DeDgoNAfIyPP9GN9oY9AI=",
+    "h1:wY9tBRLlkmwWTPxu1YsmlihwMJ17hfDNJ6zJCsM0dxk=",
   ]
 }
 
--- main.tf
+++ main.tf
@@ -4,7 +4,14 @@ terraform {
       source = "local.providers/local/terratowns"
       version = "1.0.0"
     }
-  }
+  }  
+    
+  # required_providers {
+  #   terratowns = {
+  #     source = "local.providers/local/terratowns"
+  #     version = "1.0.0"
+  #   }
+  # }
 
 # https://developer.hashicorp.com/terraform/language/modules/sources
 # module "terrahouse_aws" {
@@ -17,22 +24,24 @@ terraform {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
-
 }
 
 provider "terratowns" {
-  endpoint  = "https://localhost:4567"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
-resource "terratowns_home" "morton_manor" {
-  name = "How to play A in 2023"
-  description = <<DOC
-This is a better description on how to play A in the year 2023.
-  DOC
-  town = "missingo"
-  content_version = 1
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
   #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3e8u1j4b.cloudfront.net"
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
 }
